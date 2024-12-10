7f97262463afd17ef2a9e38e19c7caa2a763660c
baggy102 <rlgh0806@gmail.com>
2024-12-10 22:41:13 +0900

    modify/메인페이지 미디어쿼리 적용, 헤더 우측 호버 버튼 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false); 
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev); 
+  };
+
   return (
     <div>
       <header>
         <h1>Stride+</h1>
+
+        {/* 햄버거 메뉴 버튼 */}
+        <button className="menu-toggle" onClick={toggleMenu}>
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </button>
+
+        {/* 메뉴가 열렸을 때 나타날 메뉴 */}
+        {isMenuOpen && (
+          <div className="menu">
+            <a href="/signup">Signup</a>
+            <a href="/signin">Signin</a>
+          </div>
+        )}
       </header>
 
       <main>
         <img
-          src="https://via.placeholder.com/600x400/blue"
+          src={process.env.PUBLIC_URL + '/main-running-track.jpg'}
           alt="Track"
         />
 
